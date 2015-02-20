170456cd95dda73843b9917846d734c5b7c013dd
Higor Roberto <higor@streamtel.com.br>
2015-02-20 18:22:57 -0200

    Script de troca de banner

--- index.php
+++ index.php
@@ -6,9 +6,11 @@
 		<meta name="keywords" content="moveis ecologicos">
 		<link rel="stylesheet" href="estilos.css">
 		<title>Móveis Ecológicos</title>
+		<script src="principal.js"></script>
 	</head>
 	<body>
 		<h1>Móveis Ecologicos S . A.</h1>
+		<h2 id="mensagem"></h2>
 		<ul>
 			<li>Móveis de garrafa PET</li>
 			<li>Móveis de latinha de alumínio</li>
