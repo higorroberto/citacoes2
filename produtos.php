857d45fd500d80de5707af015b2aff35d92eb3c9
Higor Roberto <higor@streamtel.com.br>
2015-02-23 11:54:34 -0300

    Página de produtos

---
+++ produtos.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Produtos</title>
+	</head>
+	<body>
+		<ul>
+			<li>Móveis de garrafas PET</li>
+			<li>Móveis de latinha de alumínio</li>
+			<li>Móveis de papelão</li>
+		</ul>
+	</body>
+</html>
